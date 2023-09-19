a7b46f5c320cb9f8aa40bb1814d9992e356f4393
Shivansh Singh <singhshivansh12may@gmail.com>
2023-09-19 21:15:48 +0530

    Created city repository.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 const express=require("express");
 const {port} =require("./config/serverConfig.js");
+const CityRepository = require("./repository/city-repository.js");
+
 const setUpandStartServer=async()=>{
     const app=express();
     
     app.listen(port,function(req,res){
          console.log("Server Started at Port "+port);
-    });
+         const repo=new CityRepository();
+         repo.createCity({name :"New Delhi"});
+    }); 
 }
-setUpandStartServer()
+setUpandStartServer();
