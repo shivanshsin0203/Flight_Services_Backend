14bf489c5777b058baa00d082c77a44abf927b17
Shivansh Singh <singhshivansh12may@gmail.com>
2023-09-21 18:37:54 +0530

    Completed city-repositry

---
+++ src/services/city-service.js
@@ -0,0 +1,45 @@
+const { response } = require("express");
+const {CityRepository}=require("../repository/index")
+class CityService{
+      constructor(){
+        this.cityRepository=new CityRepository();
+      }
+      async createCity(data){
+        try {
+            const response= await this.cityRepository.createCity(data);
+            return response;
+        }catch(error){
+            console.log("An error has occured ");
+              throw{error};
+        }
+      }
+      async deleteCity(cityId){
+        try {
+            const response=await this.cityRepository.deleteCity(cityId);
+            return response;
+
+        }catch(error){
+            console.log("An error has occured ");
+              throw{error};
+        }
+      }
+      async updateCity(cityId,data){
+        try {
+            const city=await this.cityRepository.updateCity(cityId,data);
+            return city;
+        }catch(error){
+            console.log("An error has occured ");
+              throw{error};
+        }
+      }
+      async getCity(cityId){
+        try {
+           const city=await this.cityRepository.getCity(cityId);
+           return city;
+        }catch(error){
+            console.log("An error has occured ");
+              throw{error};
+        }
+      }
+}
+module.exports=CityService;
